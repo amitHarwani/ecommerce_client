58ee822fd10a43db58767c2914875432755d23e8
amitHarwani <harwania21@gmail.com>
2023-12-14 13:23:25 +0400

    Proxy and environment variables set up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default defineConfig(({ _, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    server: {
+      port: env.VITE_APP_PORT,
+      proxy: {
+        "/api": {
+          target: env.VITE_SERVER_URI,
+        },
+      },
+    },
+  };
+});
