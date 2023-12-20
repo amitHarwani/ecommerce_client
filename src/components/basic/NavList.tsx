c857f8ad60f9630bd27dec3c947588a792e380fa
amitHarwani <harwania21@gmail.com>
2023-12-20 16:17:27 +0400

    Fix RTL of NavList

--- src/components/basic/NavList.tsx
+++ src/components/basic/NavList.tsx
@@ -1,25 +1,28 @@
 import { NavigationOption } from "../../constants";
+import { useAppSelector } from "../../store";
 import NavItem from "./NavItem";
 
-
 interface NavListProps {
-    navList: Array<NavigationOption>
-    className?: string
+  navList: Array<NavigationOption>;
+  className?: string;
 }
 
 const NavList = (props: NavListProps) => {
+  const { navList, className = "" } = props;
 
-    const {navList, className = ''} = props;
+  const isRTL = useAppSelector((state) => state.language.isRTL);
 
-    return (
-        <div className={`flex flex-col lg:flex-row lg:justify-between lg:items-center ${className} `}>
-            {
-                navList.map((navItem) => (
-                    <NavItem navItem={navItem} key={navItem.id} />
-                ))
-            }
-        </div>
-    )
-}
+  return (
+    <div
+      className={`flex flex-col lg:flex-row lg:justify-between lg:items-center 
+        ${isRTL ? "lg:flex-row-reverse" : ""} 
+        ${className} `}
+    >
+      {navList.map((navItem) => (
+        <NavItem navItem={navItem} key={navItem.id} />
+      ))}
+    </div>
+  );
+};
 
 export default NavList;
